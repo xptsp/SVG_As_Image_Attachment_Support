62fab0a0cbc7e1fc8aeaa66fc86efc333fe9fc1f
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-11-08 20:24:00 -0600

    v1.0.2 - February 26th, 2016
    v1.0.2 - February 26th, 2016
    o Fixed improperly expanding thumbnail issue.
    o Minor code revision to add further performance & security enhancements.

--- Subs-SVG.php
+++ Subs-SVG.php
@@ -15,15 +15,18 @@ if (!defined('SMF'))
 //================================================================================
 // Function to add some additional information to the AttachmentData element:
 //================================================================================
-function svg_styleJS($attachment, &$element)
+function svg_styleJS(&$attachment, &$element)
 {
 	$element['thumbnail']['style_def'] = '';
-	$element['thumbnail']['href2'] = $element['thumbnail']['href'];
-	if ($attachment['thumb_mime'] == 'image/svg+xml')
+	if (isset($element['thumbnail']['href']))
 	{
-		$element['thumbnail']['href2'] .= '" class="thumb_' . $attachment['id_attach'];
-		$element['thumbnail']['style_def'] = '<style>.thumb_' . $attachment['id_attach'] . ' { width: ' . $attachment['thumb_width'] . 'px; height: ' . $attachment['thumb_height'] . 'px; }</style>';
-		$element['thumbnail']['javascript'] = 'return expandSVG(' . $attachment['id_attach'] . ');';
+		$element['thumbnail']['href2'] = $element['thumbnail']['href'];
+		if ($attachment['thumb_mime'] == 'image/svg+xml')
+		{
+			$element['thumbnail']['href2'] .= '" class="thumb_' . $attachment['id_attach'];
+			$element['thumbnail']['style_def'] = '<style>.thumb_' . $attachment['id_attach'] . ' { width: ' . $attachment['thumb_width'] . 'px; height: ' . $attachment['thumb_height'] . 'px; }</style>';
+			$element['thumbnail']['javascript'] = 'return expandSVG(' . $attachment['id_attach'] . ');';
+		}
 	}
 }
 
@@ -36,9 +39,9 @@ function svg_getimagesize($filename, $svg_only = false)
 	if (!empty($size))
 		return $svg_only ? false : $size;
 	$file = file_get_contents($filename);
-	if (preg_match('~(iframe|(?<!cellTextIs)html|eval|body|script\W|[CF]WS[\x01-\x0C])~i', $file))
-		return false;
 	if (!$file || !preg_match('~<\?xml version=(.+?)>*(<svg.+?>)~is', $file, $matches))
+		return false;
+	if (preg_match('~(iframe|(?<!cellTextIs)html|eval|body|script\W|[CF]WS[\x01-\x0C])~i', $file))
 		return false;
 	if (!preg_match('#width=(?:"?)(\d+)(?:")?#', $matches[2], $width) || !preg_match('#height=(?:"?)(\d+)(?:")?#', $matches[2], $height))
 		return false;
@@ -61,38 +64,40 @@ function svg_getimagesize($filename, $svg_only = false)
 //================================================================================
 // Function for "resizing" SVG files into attachments:
 //================================================================================
-function svg_resizeImage($destName, $src_width, $src_height, $max_width, $max_height)
+function svg_resizeImage($destName, $max_width, $max_height)
 {
+	// Read the file into memory and check to make sure it's a SVG image:
 	$file = file_get_contents($destName);
-	$pattern = '~<\?xml version=(.+?)>*(<svg.+?>)~is';
-	if (!$file || !preg_match($pattern, $file, $matches))
+	if (!$file || !preg_match('~<\?xml version=(.+?)>*(<svg.+?>)~is', $file, $matches))
 		return false;
+	if (preg_match('~(iframe|(?<!cellTextIs)html|eval|body|script\W|[CF]WS[\x01-\x0C])~i', $file))
+		return false;
+	if (!preg_match('#width=(?:"?)(\d+)(?:")?#', $matches[2], $width) || !preg_match('#height=(?:"?)(\d+)(?:")?#', $matches[2], $height))
+		return false;
+	$src_width = $width[1];
+	$src_height = $height[1];
 	$msg = $matches[2];
 
 	// Determine whether to resize to max width or to max height (depending on the limits.)
-	if (!empty($max_width) || !empty($max_height))
+	if (empty($max_width) && empty($max_height))
+		return false;
+	if (!empty($max_width) && (empty($max_height) || $src_height * $max_width / $src_width <= $max_height))
 	{
-		if (!empty($max_width) && (empty($max_height) || $src_height * $max_width / $src_width <= $max_height))
-		{
-			$dst_width = $max_width;
-			$dst_height = floor($src_height * $max_width / $src_width);
-		}
-		elseif (!empty($max_height))
-		{
-			$dst_width = floor($src_width * $max_height / $src_height);
-			$dst_height = $max_height;
-		}
-
-		// Don't bother resizing if it's already smaller...
-		if (!empty($dst_width) && !empty($dst_height) && ($dst_width < $src_width || $dst_height < $src_height || $force_resize))
-		{
-			if (preg_match('#width=(?:"?)(\d+)(?:")?#', $matches[2], $width))
-				$msg = str_replace($width[0], $width[0] . ' width-smf_thumb="' . $dst_width . '"', $msg);
-			if (preg_match('#height=(?:"?)(\d+)(?:")?#', $matches[2], $height))
-				$msg = str_replace($height[0], $height[0] . ' height-smf_thumb="' . $dst_height . '"', $msg);
-			$file = str_replace($matches[2], $msg, $file);
-		}
+		$dst_width = $max_width;
+		$dst_height = floor($src_height * $max_width / $src_width);
 	}
+	elseif (!empty($max_height))
+	{
+		$dst_width = floor($src_width * $max_height / $src_height);
+		$dst_height = $max_height;
+	}
+	if (!(!empty($dst_width) && !empty($dst_height) && ($dst_width < $src_width || $dst_height < $src_height)))
+		return false;
+	
+	// Modify the SVG tag to include the resized image dimensions:
+	$msg = preg_replace('#width=(?:"?)(\d+)(?:")?#', '$0 width-smf_thumb="' . $dst_width . '"', $msg);
+	$msg = preg_replace('#height=(?:"?)(\d+)(?:")?#', '$0 height-smf_thumb="' . $dst_height . '"', $msg);
+	$file = str_replace($matches[2], $msg, $file);
 
 	// Save the image as ...
 	$fp_destination = fopen($destName, 'wb');
@@ -129,20 +134,21 @@ if (class_exists('Imagick') && (function_exists('imagecreatefrompng') || functio
 //================================================================================
 // Function for dealing with unthumbnailed existing SVG image attachments:
 //================================================================================
-function svg_findDimensions($pm = false)
+function svg_findDimensions($pm = false, $all = false)
 {
 	global $smcFunc;
 
 	isAllowedTo('manage_attachments');
 	$table = $pm ? 'pm_attachments' : 'attachments';
+	$fname = $pm ? 'getPMAttachmentFilename' : 'getAttachmentFilename';
 
 	// We're looking for attached SVG images that don't have the dimensions/mime type:
 	$request = $smcFunc['db_query']('', '
 		SELECT *
 		FROM {db_prefix}' . $table . '
-		WHERE filename LIKE {string:svg_image}
+		WHERE filename LIKE {string:svg_image}' . ($all ? '
 			AND width = {int:zero}
-			AND height = {int:zero}',
+			AND height = {int:zero}' : ''),
 		array(
 			'svg_image' => '%.svg',
 			'zero' => 0,
@@ -151,10 +157,7 @@ function svg_findDimensions($pm = false)
 	while ($row = $smcFunc['db_fetch_assoc']($request))
 	{
 		// Get the image size information for the attachment:
-		if ($pm)
-			$filename = getPMAttachmentFilename($row['filename'], $row['id_attach'], $row['id_folder'], false, $row['file_hash']);
-		else
-			$filename = getAttachmentFilename($row['filename'], $row['id_attach'], $row['id_folder'], false, $row['file_hash']);
+		$filename = $fname($row['filename'], $row['id_attach'], $row['id_folder'], false, $row['file_hash']);
 		$sizes = @svg_getimagesize($filename, true);
 
 		// If this is a SVG image, update the attachment table so that the thumbs can be generated:
@@ -193,10 +196,10 @@ function svg_removeDimensions($pm = false)
 	$smcFunc['db_query']('', '
 		UPDATE {db_prefix}' . ($pm ? 'pm_' : '') . 'attachments
 		SET width = {int:zero}, height = {int:zero}
-		WHERE filename LIKE {string:svg_image}',
+		WHERE mime_type LIKE {string:svg_mime}',
 		array(
 			'zero' => 0,
-			'svg_image' => '%.svg',
+			'svg_mime' => 'image/svg+xml',
 		)
 	);
 	redirectExit('action=admin;area=manageattachments;sa=maintenance');
